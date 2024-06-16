4f257b84f8c3260f6e658a5b907692559ea8a882
aRanHC <aranhc@163.com>
2024-06-16 19:07:38 +0800

    新增前端登录校验以及登录过滤器

--- src/com/atguigu/headline/controller/NewsUserController.java
+++ src/com/atguigu/headline/controller/NewsUserController.java
@@ -26,11 +26,13 @@ import java.util.Map;
  * @Create 2024/6/10 21:19
  */
 @WebServlet("/user/*")
-public class NewsUserController extends BaseController{
+public class NewsUserController extends BaseController {
 
     private NewsUserService userService = new NewsUserServiceImpl();
+
     /**
      * 处理登录表单提交的业务接口
+     *
      * @param req
      * @param resp
      * @throws ServletException
@@ -42,50 +44,52 @@ public class NewsUserController extends BaseController{
         //调用服务层方法，实现登录
         NewsUser loginUser = userService.findByUsername(paramUser.getUsername());
         Result result = null;
-        if (null != loginUser){
-            if (MD5Util.encrypt(paramUser.getUserPwd()).equalsIgnoreCase(loginUser.getUserPwd())){
+        if (null != loginUser) {
+            if (MD5Util.encrypt(paramUser.getUserPwd()).equalsIgnoreCase(loginUser.getUserPwd())) {
                 Map data = new HashMap();
-                data.put("token",JwtHelper.createToken(loginUser.getUid().longValue()));
+                data.put("token", JwtHelper.createToken(loginUser.getUid().longValue()));
 //                result = Result.build(data,ResultCodeEnum.SUCCESS);
                 result = Result.ok(data);
-            }else {
-                result = Result.build(null,ResultCodeEnum.PASSWORD_ERROR);
+            } else {
+                result = Result.build(null, ResultCodeEnum.PASSWORD_ERROR);
             }
-        }else {
-            result = Result.build(null,ResultCodeEnum.USERNAME_ERROR);
+        } else {
+            result = Result.build(null, ResultCodeEnum.USERNAME_ERROR);
         }
         //向客户端响应登录验证信息
-        WebUtil.writeJson(resp,result);
+        WebUtil.writeJson(resp, result);
     }
 
     /**
-     *  根据token口令获取用户详细信息
+     * 根据token口令获取用户详细信息
+     *
      * @param req
      * @param resp
      * @throws ServletException
      * @throws IOException
      */
     protected void getUserInfo(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Result result = Result.build(null,ResultCodeEnum.NOTLOGIN);
+        Result result = Result.build(null, ResultCodeEnum.NOTLOGIN);
         //获取token
         String token = req.getHeader("token");
-        if (null != token && (!"".equals(token))){
-            if (!JwtHelper.isExpiration(token)){
+        if (null != token && (!"".equals(token))) {
+            if (!JwtHelper.isExpiration(token)) {
                 Integer userId = JwtHelper.getUserId(token).intValue();
                 NewsUser newsUser = userService.findByUid(userId);
-                if (null != newsUser){
+                if (null != newsUser) {
                     Map data = new HashMap();
                     newsUser.setUserPwd("");
-                    data.put("loginUser",newsUser);
+                    data.put("loginUser", newsUser);
                     result = Result.ok(data);
                 }
             }
         }
-        WebUtil.writeJson(resp,result);
+        WebUtil.writeJson(resp, result);
     }
 
     /**
      * 用户名占用校验
+     *
      * @param req
      * @param resp
      * @throws ServletException
@@ -95,17 +99,18 @@ public class NewsUserController extends BaseController{
         Result result = null;
         String username = req.getParameter("username");
         NewsUser findByUsernameUser = userService.findByUsername(username);
-        if (null != findByUsernameUser){
+        if (null != findByUsernameUser) {
             //找到同名的
-            result = Result.build(null,ResultCodeEnum.USERNAME_USED);
-        }else {
+            result = Result.build(null, ResultCodeEnum.USERNAME_USED);
+        } else {
             result = Result.ok(null);
         }
-        WebUtil.writeJson(resp,result);
+        WebUtil.writeJson(resp, result);
     }
 
     /**
      * 完成注册业务接口
+     *
      * @param req
      * @param resp
      * @throws ServletException
@@ -113,10 +118,29 @@ public class NewsUserController extends BaseController{
      */
     protected void regist(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Result result = Result.ok(null);
-        NewsUser newNewsUser = WebUtil.readJson(req,NewsUser.class);
+        NewsUser newNewsUser = WebUtil.readJson(req, NewsUser.class);
         boolean isRegist = userService.isRegist(newNewsUser);
-        if (!(isRegist)){
-            result = Result.build(null,ResultCodeEnum.USERNAME_USED);
+        if (!(isRegist)) {
+            result = Result.build(null, ResultCodeEnum.USERNAME_USED);
+        }
+        WebUtil.writeJson(resp, result);
+    }
+
+    /**
+     * 前端自己校验是否处于登录状态
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    protected void checkLogin(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String token = req.getHeader("token");
+        Result result = Result.build(null,ResultCodeEnum.NOTLOGIN);
+        if (null != token) {
+            if (JwtHelper.isExpiration(token)){
+                result = Result.ok(null);
+            }
         }
         WebUtil.writeJson(resp,result);
     }
